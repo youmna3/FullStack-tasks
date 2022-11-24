8db1175b83278f47ee5c67adfdfa2a6dbacecd5b
youmna3 <youmna54@gmail.com>
2022-11-25 00:33:54 +0200

    last commit

--- S_JS_01- Add Product Component/app.js
+++ S_JS_01- Add Product Component/app.js
@@ -1,3 +1,11 @@
-"use_strict";
-document.getElementById("products").innerHTML +=
-  "<tr><td>fossil</td><td>$500</td><td>2</td><td>$100</td><td>Remove</td></tr><tr><td>fossil</td><td>$500</td><td>2</td><td>$100</td><td>Remove</td></tr>";
+// "use_strict";
+const product = document.getElementById("product-name");
+const price = document.getElementById("price");
+const quantity = document.getElementById("quantity");
+
+document.getElementById("add").addEventListener("click", () => {
+  const total = Number(price.value) * Number(quantity.value);
+  document.getElementById(
+    "products"
+  ).innerHTML += `<tr><td>${product.value}</td><td>${price.value}</td><td>${quantity.value}</td><td>${total}</td><td>Remove</td></tr>`;
+});
