174ac67b694cc1c1833d35cf210a61fc75846641
youmna3 <youmna54@gmail.com>
2022-11-24 21:05:55 +0200

    create a table and style it

--- S_JS_01- Add Product Component/app.js
+++ S_JS_01- Add Product Component/app.js
@@ -1,1 +1,3 @@
-"use strict";
+"use_strict";
+document.getElementById("products").innerHTML +=
+  "<tr><td>fossil</td><td>$500</td><td>2</td><td>$100</td><td>Remove</td></tr><tr><td>fossil</td><td>$500</td><td>2</td><td>$100</td><td>Remove</td></tr>";
