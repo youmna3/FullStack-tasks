864b5fc67217ba5c95e03c2ca4edc2167c92c109
youmna3 <youmna54@gmail.com>
2022-11-28 15:43:44 +0200

    edit js code

--- S_JS_01- Add Product Component/app.js
+++ S_JS_01- Add Product Component/app.js
@@ -1,10 +1,10 @@
 // "use_strict";
-const product = document.getElementById("product-name");
-const price = document.getElementById("price");
-const quantity = document.getElementById("quantity");
 
 document.getElementById("add").addEventListener("click", () => {
-  const total = Number(price.value) * Number(quantity.value);
+  const product = document.getElementById("product-name");
+  const price = document.getElementById("price");
+  const quantity = document.getElementById("quantity");
+  const total = price.value * quantity.value;
   document.getElementById(
     "products"
   ).innerHTML += `<tr><td>${product.value}</td><td>${price.value}</td><td>${quantity.value}</td><td>${total}</td><td>Remove</td></tr>`;
