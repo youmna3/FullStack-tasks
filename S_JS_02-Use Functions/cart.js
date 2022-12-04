3f5f80ec3bec32b280a09642a5811e94b5167466
youmna3 <youmna54@gmail.com>
2022-12-04 20:35:16 +0200

    create func to calc subtotal

--- S_JS_02-Use Functions/cart.js
+++ S_JS_02-Use Functions/cart.js
@@ -1,49 +1,52 @@
 "use strict";
 
-//let product = [];
-// const shipping = () => {
-//   product * 15;
-// };
-//document.querySelector(".model");
-/*
-const tableRow = document.querySelectorAll(".row");
-for (let i = 0; i < tableRow.length; i++) {
-  let removeBtn = document
-    .getElementById("remove")
-    .addEventListener("click", () => {
-      tableRow[i].classList.remove("row");
-    });
-}
-*/
-// const tableRow = document.querySelectorAll(".row");
-// let removeBtn = document
-//   .getElementById("remove")
-//   .addEventListener("click", () => {
-//     document.getElementsByTagName("tr").classList.remove();
-//   });
+// const shippingCost = 15;
+// const shipping = (document.getElementById(
+//   "shipping"
+// ).textContent = `$${shippingCost}`);
+//document.getElementById("total").textContent = `$${subTotal + shippingCost}`;
 document.getElementById("add").addEventListener("click", () => {
-  const product = document.getElementById("product-name");
-  const price = document.getElementById("price");
-  const quantity = document.getElementById("quantity");
-  const total = price.value * quantity.value;
-  let removeBtn = document
-    .getElementById("remove")
-    .addEventListener("click", () => {});
-  document.getElementById("products").innerHTML += `<tr>
-  <td>${product.value}</td>
-  <td>${price.value}</td>
-  <td><button type="button">-</button><input type="number" value="${quantity.value}" /><button type="button">+</button></td>
-  <td>${total}</td>
-  <td>${removeBtn}</td></tr>`;
-  document.getElementById("sub-total").textContent = `$${total}`;
+  const product = document.getElementById("product-name").value;
+  const price = document.getElementById("price").value;
+  const quantity = document.getElementById("quantity").value;
+  let subTotal = price * quantity;
 
-  const shipping = (document.getElementById("shipping").textContent = 15);
-  //document.getElementById("shipping").innerHTML = `$${shipping()}`;
-  document.getElementById("total").textContent = `$${total + shipping}`;
-  if (!product.value || !price.value || !quantity.value) {
+  document.querySelector(".products").innerHTML += productRow(
+    product,
+    price,
+    quantity,
+    subTotal
+  );
+  calcTotal();
+  // document.getElementById("sub-total").textContent = `$${subTotal}`;
+  //const shipping = (document.getElementById("shipping").textContent = 15);
+
+  if (!product || !price || !quantity) {
     alert("please enter a valid input");
   }
 });
+
+const calcTotal = () => {
+  const productsArray = document.getElementsByClassName("product-total");
+  let total = 0;
+  // for (let i = 0; i < productsArray.length; i++) {
+  //   total += Number(parseFloat(productsArray[i].innerHTML.replace("$", "")));
+  // }
+  for (const e of productsArray) {
+    total += Number(parseFloat(e.innerHTML.replace("$", "")));
+  }
+  document.getElementById("sub-total").innerHTML = `${total}`;
+};
+
+const productRow = (product, price, quantity, subTotal) => {
+  return `<tr>
+  <td>${product}</td>
+  <td>${price}</td>
+  <td><button type="button">-</button><input type="number" value="${quantity}" /><button type="button">+</button></td>
+  <td class="product-total">${subTotal}</td>
+  <td><button type="button">Remove</button></td></tr>`;
+};
+
 // const style = (s) => {
 //   document.querySelector(".check-btn").style = s;
 // };
@@ -51,4 +54,16 @@ document.querySelector(".check-btn").style.backgroundColor = "#6a2929";
 document.querySelector(".check-btn").style.padding = "5px";
 document.querySelector(".check-btn").style.margin = "5px";
 document.querySelector(".check-btn").style.cursor = "pointer";
+/*
+document.querySelector(".hidden").style.display = "none";
+const deleteProd = () => {
+  addProd.classList.add("hidden");
+};
+
+const removeBtn = document
+  .getElementById("remove")
+  .addEventListener("click", deleteProd);
+
+*/
+
 //set local storage
