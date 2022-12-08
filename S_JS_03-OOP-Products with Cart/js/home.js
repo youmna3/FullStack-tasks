7f9ae5954f27501e77db65dd6a9d2c408236f700
youmna3 <youmna54@gmail.com>
2022-12-08 17:39:15 +0200

    first commit in OOP task

---
+++ S_JS_03-OOP-Products with Cart/js/home.js
@@ -0,0 +1,11 @@
+"use strict";
+const addToCart = (product) => {
+  const products = JSON.parse(localStorage.getItem("products") || "[]");
+  const oldP = products.findIndex((x) => x.id === product.id);
+  if (oldP >= 0) {
+    products[oldP].quantity += 1;
+  } else {
+    products.push({ ...product, quantity: 1 });
+  }
+  localStorage.setItem("products", JSON.stringify(products));
+};
