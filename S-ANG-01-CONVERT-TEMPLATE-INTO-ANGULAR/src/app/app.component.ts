b072f6665a6905be955fe2cd3637818092173ade
youmna3 <youmna54@gmail.com>
2022-12-09 15:53:58 +0200

    angular first commit

---
+++ S-ANG-01-CONVERT-TEMPLATE-INTO-ANGULAR/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'S-ANG-01-CONVERT-TEMPLATE-INTO-ANGULAR';
+}
