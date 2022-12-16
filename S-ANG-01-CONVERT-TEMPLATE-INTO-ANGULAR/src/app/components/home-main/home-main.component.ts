c5e1f16e6c808977771e5b9083c866b3735c23a4
youmna3 <youmna54@gmail.com>
2022-12-16 04:00:56 +0200

    added fav btn

--- S-ANG-01-CONVERT-TEMPLATE-INTO-ANGULAR/src/app/components/home-main/home-main.component.ts
+++ S-ANG-01-CONVERT-TEMPLATE-INTO-ANGULAR/src/app/components/home-main/home-main.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-main',
   templateUrl: './home-main.component.html',
   styleUrls: ['./home-main.component.css'],
 })
-export class HomeMainComponent {}
+export class HomeMainComponent {
+  faHeart = faHeart;
+  isDisabled: boolean = false;
+  handelClick() {
+    alert('added to favorites');
+  }
+}
